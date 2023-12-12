e80d852fbcba90ef315e41e6502b521969a14f76
r200049 <70116724+r200049@users.noreply.github.com>
2023-12-12 14:37:31 -0600

    Add files via upload

--- entry.js
+++ entry.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   subscribeToSubmit();
 });
 
+
 function subscribeToSubmit() {
   /* Subscribe to submit-button click event */
   document.getElementById("submit-button").addEventListener("click", () => {
@@ -10,42 +11,39 @@ function subscribeToSubmit() {
     const name = nameInput.value;
     nameInput.value = '';
 
+
     const liftInput = document.getElementById('lift-type-input');
     const lift = liftInput.value;
     liftInput.value = '';
 
-    const weightInput = document.getElementById('weight-input');  
-    const weight = weightInput.value;
-    weightInput.value = '';
 
-    //const kgInput = document.getElementById('kg-input');  
-    //const kg = kgInput.value;
-    //kgInput.value = '';
+    const weightInput = document.getElementById('weight-input');  
+    const weightLbs = weightInput.value;
+    weightInput.value = '';
+   
+    const weightKg = (weightLbs /2.20462262).toFixed(0);
+
 
     const repsInput = document.getElementById('reps-input');
     const reps = repsInput.value;
     repsInput.value = '';
-    
-    const dateInput = document.getElementById('date-input')
-    const date = dateInput.value;
-    dateInput.value = '';
 
-    const combined = (weight / (1.0278 - (0.0278 * reps))).toFixed(2);
-    const kilograms = (weight /2.20462262).toFixed(0);
+
+    const combined = (weightLbs / (1.0278 - (0.0278 * reps))).toFixed(2);
+    
 
     const body = {
-      weight: weight,
-      lift: lift,
-      reps: reps,
       name: name,
-      combined: combined,
-      date: date,
-      kilograms: kilograms
+      lift: lift,
+      weightLbs: weightLbs,
+      weightKg: weightKg,
+      reps: reps,
+      combined: combined
     };
-    
+   
     console.log(body);
-    
-    fetch('http://127.0.0.1:8082', {
+   
+    fetch('http://127.0.0.1:8083', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- landing.js
+++ landing.js
@@ -1,111 +1,112 @@
 var table;
-var tabledata;
+var tableData;
 
-document.addEventListener("DOMContentLoaded", function(event) { // makes sure HTML page is loaded
-     table = document.getElementById("leaderboard");
-    
-    fetch('http://localhost:8082')
+console.log('mango')
+/* Verify HTML content is loaded */
+document.addEventListener("DOMContentLoaded", function(event) {
+  table = document.getElementById("leaderboard");
+   
+  fetch("http://127.0.0.1:8083")
     .then(response => response.json())
     .then(data => {
       tableData = data;
       generateTable(table, data);
+      sortTable("Name");
       //createChart(data);
   });
-  
+ 
   document.getElementById("sort-dropdown").addEventListener("change", (event) => {
     sortTable(event.target.value);
   });
+ 
   document.getElementById("login-button").addEventListener("click", (event) => {
     filterTable(document.getElementById("login-field").value);
   });
-  
 });
 
 
-
-function filterTable(name) {
-  if (this.tableData != null) {
-    let sortedData = tableData.filter((element) => {
-      return element.Name == name;
-    });
-    this.table.innerHTML = "";
-    generateTable(table, sortedData);
-    createChart(sortedData)
-  }
-}
-
-
-
 function generateTable(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key in data[0]) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
+  /* Insert table header */
+  const tableColumns = ["Name", "Date", "Lift", "Weight (lbs)", "Weight (kg)", "Reps", "Combined"];
+  let tableHead = table.createTHead();
+  let tableRow = tableHead.insertRow();
+  for (let column of tableColumns) {
+    let text = document.createTextNode(column);
+    let cell = document.createElement("th");
+    cell.appendChild(text);
+    tableRow.appendChild(cell);
   }
-  
-  for (let element of data) {
-    let row = table.insertRow();
-    for (let key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
+ 
+  /* Insert table rows */
+  for (let row of data) {
+    let tableRow = table.insertRow();
+    for (let column of tableColumns) {
+      let text = document.createTextNode(row[column]);
+      let cell = tableRow.insertCell();
       cell.appendChild(text);
     }
   }
 }
 
-/*function sortTable(sortField) {
-  if (this.tableData != null) {
-    let sortedData = tableData.sort((a,b) => {
-      return a[sortField] - b[sortField];
-    });
-    this.table.innerHTML = "";
-    generateTable(table, sortedData);
-  }
-}
-*/
 
 function sortTable(sortField) {
-  if (this.tableData != null) {
-    let sortedData = tableData.sort((a,b) => {
+  if (this.tableData == null) { return; }
+ 
+  let sortedData = tableData.sort((a,b) => {
+    if (sortField == "Name" || sortField == "Lift") {
+      return b[sortField].localeCompare(a[sortField]);
+    } else {
       return b[sortField] - a[sortField];
-    });
-    this.table.innerHTML = "";
-    generateTable(table, sortedData);
-    
-  }
+    }
+  });
+ 
+  this.table.innerHTML = "";
+  generateTable(table, sortedData);
 }
+
+
+function filterTable(name) {
+  if (this.tableData == null) { return; }
+ 
+  let sortedData = tableData.filter((element) => {
+    return element.Name == name;
+  });
+   
+  this.table.innerHTML = "";
+  generateTable(table, sortedData);
+  createChart(sortedData)
+}
+
 
 function createChart(data) {
   var xyValues = [];
-  
+ 
   for (let element of data) {
-    // Parse date string directly to Date 
-    var date = new Date(element.Date);
+    /* Parse date string directly to Date */
+    var date = new Date(element.Date + "T24:00:00");
     var timestamp = date.getTime();
-    xyValues.push({x: timestamp, y: element.Combined});
+    xyValues.push({x: timestamp, y: element["Combined"]});
   }
-  
-	new Chart("myChart", {
-		type: "line",
-		data: {
-			datasets: [{
-				pointRadius: 4,
-				pointBackgroundColor: "rgb(0,0,255)",
-				data: xyValues
-			}]
-		},
-		options: {
-			legend: {display: false},
-			scales: {
-				xAxes: [{type : 'time', time:{unit:'day'}}]
-				//yAxes: [{ticks: {min: 0, max:1000}}],
-			}
-		}
-	});
+ 
+  new Chart("lift-chart", {
+    type: "line",
+    data: {
+      datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        xAxes: [{type : 'time', time:{unit:'day'}}]
+        //yAxes: [{ticks: {min: 0, max:1000}}],
+      }
+    }
+  });
 }
+
 
 
 
